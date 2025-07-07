4b3454a17b95daa734f6183bc02e5f43ceb91a74
souarbhjeengar <souarbhrathore50@gmail.com>
2025-07-07 20:19:16 +0530

    create tables and rolepermission  package

---
+++ database/migrations/2025_07_07_143826_create_invitations_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invitations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('inviter_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('company_id')->nullable()->constrained('companies')->onDelete('cascade');
+            $table->string('email');
+            $table->string('role'); // Admin, Member, etc.
+            $table->string('token')->unique();
+            $table->timestamp('expires_at')->nullable();
+            $table->boolean('accepted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invitations');
+    }
+};
